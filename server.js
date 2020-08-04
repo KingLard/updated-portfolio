573ed1cee23cb6c037357594f1a097ddc0ac3af9
Brett Landes <brettl0407@gmail.com>
2020-08-03 20:51:22 -0400

    added static server to server.js

--- server.js
+++ server.js
@@ -15,9 +15,17 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3000
 
+if (process.env.NODE_ENV === 'production') {
+	app.use(express.static('client/build'));
+}
+
 app.use(express.static("public"));
 app.use(express.json({limit: '1mb'}))
 
+
+app.get('*', (request, response) => {
+	response.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+});
 
 
 app.post('/myform', function(req, res) {
